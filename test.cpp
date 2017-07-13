a37fbd0b7e98d05b62c75e9df1fe8e741014375d
fysicsguy <squirmis@gmail.com>
2017-07-13 12:21:41 -0400

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,34 +1,101 @@
-//This will be my first shitty github code
-
 #include <iostream>
 #include <string>
 #include <iomanip>
 
 
 
+int burger_choice;
+int drink_choice;
+float burger_price;
+float drink_price;
+float price;
+float tax;
+float total;
+
 int main()
 	{
-	char quit = 'n';
-	while (quit == 'n')
-		{
-		int burger_choice;
-		int drink_choice;
-		char Answer; 
-		std::cout << "Welcome to Burger King, Please select what you want to eat\n";
-		std::cout << "Please select a burger \n1)Hamburger \n2)Cheeseburger \n3)BaconBurger\n";
+	char choice;
+	bool run = true;
+	
+	
+	std::cout << "Welcome to Burger King, Please select what you want to eat\n";
+	while(run)
+	 {
+	
+		
+		std::cout << "Please select a burger \n1)Hamburger			$2.00 \n2)Cheeseburger			$2.50 \n3)BaconBurger			$2.50\n";
 		std::cin >> burger_choice;
-		if (burger_choice == 1)
-			std::cout << "You have selected a Hamburger.\n";
-		if (burger_choice == 2)
-			std::cout << "You have selected a Cheeseburger.\n";
-		if (burger_choice == 3)
-			std::cout << "You have selected a Baconburger.\n";
-		else std::cout << "Incorrect value selected\n";
-			  std::cout << "Please select either '1, 2, or 3'\n";
-		return 0;	
 		
+		if (std::cin.fail()){					
+			std::cin.clear();
+			std::cin.ignore(10, '\n');
+			std::cout << "Please Select a Valid Option.\n";
 		}
-		std::cin >> quit;
+					
 		
+		if (burger_choice == 1){
+			std::cout << "You have selected a Hamburger.\n";
+			burger_price = 2.00;}
+		else if (burger_choice == 2){
+			std::cout << "You have selected a Cheeseburger.\n";	
+			burger_price = 2.50;}	
+		else if (burger_choice == 3){
+			std::cout << "You have selected a Baconburger.\n";
+			burger_price = 2.50;}
+		else {continue;}
+			
+			
+			
+			
+						
+			
+		std::cout << "Please select a drink \n1)Sprite			$1.00 \n2)Coke			$1.00 \n3)Tequila			$3.00\n";	
+		std::cin >> drink_choice;
+		
+		if (std::cin.fail()){					
+			std::cin.clear();
+			std::cin.ignore(10, '\n');
+			std::cout << "Please Select a Valid Option.\n";
+		}
+		
+		while (drink_choice != 1 and drink_choice != 2 and drink_choice != 3 or drink_choice == 0 ){
+		std::cout << "Invalid Selection\n";
+		std::cin.clear();
+		if(!(std::cin >> drink_choice)){
+			std::cin.clear();
+			std::cin.ignore(10, '\n');}}
+		
+		
+		
+		if (drink_choice == 1){
+			std::cout << "You have selected a Sprite.\n";
+			drink_price = 1.00;}
+		else if (drink_choice == 2){
+			std::cout << "You have selected a Coke.\n";
+			drink_price = 1.00;}
+		else if (drink_choice == 3){
+			std::cout << "You have selected Tequila.\n";
+			drink_price = 3.00;}
+		else {continue;	
+			 }
+			 
+			
+			 
+			price = burger_price + drink_price; 
+			tax = price * 0.08;					
+			total = price + tax;				
+		std::cout << "Your total comes to $" << total <<"\n";
+		std::cout << "Thank you \nHave a nice Day\n";	
+		std::cout << "Next customer? (Y/N)\n";
+		std::cin >> choice;
+		if(choice =='N'||choice =='n'){
+            run = false;
 		
 	}
+	}	
+			return 0;	
+		
+		
+		
+}		
+	
